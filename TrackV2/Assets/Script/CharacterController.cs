356f77b865160440708725c6d5e983ff5a2856d3
de-lujo <hip-hop_de-lujo@hotmail.com>
2015-11-01 22:11:37 -0300

    Track & Field -Personaje moviéndose y la cámara lo sigue
    Algunas veces el personaje no se ve, porque queda atrás de la imagen de
    fondo.
    
    Hay que arreglar las animaciones en animator (estados).

---
+++ TrackV2/Assets/Script/CharacterController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	public float jumpForce = 350f;
+
+	bool facingRight = true;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(move));
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+	
+		GetComponent<Rigidbody2D>().velocity = new Vector2(move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        anim.SetBool("ground", grounded);
+
+		/*if(grounded && Input.GetButtonDown("Jump")){
+
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpForce));
+            anim.SetBool("ground", grounded);
+
+        }*/
+
+        if (move>0 && !facingRight)
+			Flip();
+		else if(move<0 && facingRight)
+			Flip();
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
